a25d46c2e85bba7233b91b73a1775b992eac9a41
cooljasonmelton <cooljasonmelton>
2020-11-12 19:46:54 -0600

    i ts and made to do app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import './App.css';
 
 const App = () => {
   const [toDos, setToDos] = useState([])
+  const [input, setInput] = useState("")
+
+  const handleAdd = () => {
+    setToDos([...toDos, input])
+    setInput("")
+  }
+  const handleDelete = dtd => setToDos(toDos.filter(td => td !== dtd))
+  const handleEnter = key =>  key === "Enter" ? handleAdd() : null
+
   return (
     <div className="App">
+      <div>
+        <h4>new to do</h4>
+        <input type="text" onChange={e=>setInput(e.target.value)} onKeyDown={e => handleEnter(e.key)} value={input}/>
+        <button onClick={handleAdd}>add</button>
+      </div>
 
+      <h1>to do list</h1>
+      <ul>
+        {toDos.map(td => <li>
+            {td + "  "}
+            <button onClick={() => handleDelete(td)}>X</button>
+          </li>)} 
+      </ul>
     </div>
   );
 }
